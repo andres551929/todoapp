370c8dae90e3fb26f254c060433c01df85728138
Andres <andrespaez123@hotmail.com>
2024-01-14 13:04:25 -0500

    commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, Injector, computed,effect,inject, signal } from '@angular/core';
 import { Task } from './../../models/task.model';
 import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -11,18 +11,21 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  tasks = signal<Task[]>([
-    {
-      id: Date.now(),
-      title: 'crear proyecto',
-      completed: false
-    },
-    {
-      id: Date.now(),
-      title: 'crear componentes',
-      completed: false
-    },
-  ]);
+  tasks = signal<Task[]>([]);
+
+  filter = signal('all');
+  taskByFilter = computed( () => {
+    const filter = this.filter();
+    const tasks = this.tasks();
+    if(filter === 'pending') {
+      return tasks.filter(task => !task.completed);
+    }
+    if (filter === 'completed') {
+      return tasks.filter(task => task.completed);
+
+    }
+    return tasks;
+  });
 
   newTaskCtrl = new FormControl('', {
     nonNullable: true,
@@ -30,6 +33,29 @@ export class HomeComponent {
 
     ]
   });
+
+  injector = inject(Injector);
+
+  constructor () {
+
+  }
+
+  ngOnInit(){
+    const storage = localStorage.getItem('tasks');
+    if (storage) {
+      const tasks = JSON.parse(storage);
+      this.tasks.set(tasks);
+    }
+    this.trackTasks();
+  }
+
+  trackTasks(){
+    effect(() => {
+      const tasks = this.tasks();
+      console.log(tasks);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, {injector: this.injector});
+  }
 
   changeHandler() {
     if (this.newTaskCtrl.valid && this.newTaskCtrl.value.trim() != '') {
@@ -68,6 +94,35 @@ export class HomeComponent {
     this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
   }
 
+  updateTaskEditingMode(index: number) {
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task, editing: true
+          }
+        }
+        return { ...task, editing: false };
+      })
+    });
+  }
+  updateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task, title: input.value,
+            editing:false
+          }
+        }
+        return task;
+      })
+    });
+  }
 
+  changeFilter(filter: string){
+    this.filter.set(filter);
+  }
 }
 
